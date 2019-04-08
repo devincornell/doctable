78dd4e9e7e807354ccf56db89613a137c3e4dc20
Devin Cornell <devinj.cornell@gmail.com>
2019-04-08 08:34:40 -0700

    Adding required package list.

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 
 setup(name='doctable',
     version='0.1',
-    description='Management of single table for text document storage.',
+    description='Python interface for single-table sqlite databases that allow for easy conversion to Pandas DataFrames.',
     url='https://github.com/devincornell/doctable',
     author='Devin J. Cornell',
     author_email='devinj.cornell@gmail.com',
     license='MIT',
     packages=['doctable'],
-    zip_safe=False)
+    requires=['sqlite3', 'pickle', 'pandas'],
+    zip_safe=False,
+     )
 
 
