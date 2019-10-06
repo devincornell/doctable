36398bd890d8c7a5b5dfe6ee92170345d7ddafd8
Devin J. Cornell <devinj.cornell@gmail.com>
2019-10-06 15:51:31 -0400

    Renaming doctable1 example files.

--- examples/advanced.py
+++
@@ -1,8 +0,0 @@
-import email
-from example_helper import get_sklearn_newsgroups # for this example
-
-from doctable import DocTable # this will be the table object we use to interact with our database.
-
-ddf = get_sklearn_newsgroups()
-print(ddf.shape)
-ddf.head(3)
