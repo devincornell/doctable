708cf94e960d0ff10641d023b5cfc7fd2bc8fc27
Devin J. Cornell <devinj.cornell@gmail.com>
2019-10-06 15:28:07 -0400

    Finished all examples.

--- examples/dt2_multitable.py
+++ examples/dt2_multitable.py
@@ -5,25 +5,47 @@ from time import time
 
 import sys
 sys.path.append('..')
-from doctable import DocTable2, func, op
+import doctable as dt
 
-class MyDocuments(DocTable2):
+class DocsWithSents(dt.DocTable2):
     tabname = 'mydocuments'
     schema = (
         ('id','integer',dict(primary_key=True, autoincrement=True)),
         ('name','string', dict(nullable=False, unique=True)),
-        ('age','integer'),
-        ('description','subdocs'),
+        ('age','float'),
     )
     
-    def __init__(self, fname=':memory:', engine='sqlite', verbose=False):
+    senttable = 'docsents'
+    fkcols = (['fkid'],['{}.id'.format(tabname)])
+    sentschema = (
+        ('id','integer',dict(primary_key=True, autoincrement=True)),
+        ('fkid','integer'),
+        (fkcols,'foreignkey_constraint',dict(),dict(onupdate="CASCADE", ondelete="CASCADE")),
+        ('sent','subdocs'),
+    )
+    
+    def __init__(self, fname, verbose=False):
         super().__init__(
             self.schema, 
             tabname=self.tabname, 
-            fname=fname,
-            engine=engine,
+            fname=fname, 
             verbose=verbose,
         )
+        
+        self.sentdt = dt.DocTable2(
+            schema=self.sentschema,
+            tabname=self.senttable,
+            fname=fname,
+            persistent_conn=False,
+            new_db=False,
+        )
+        
+    def insert_single(row,sents,*args,**kwargs):
+        newid = self.next_id()
+        r = self.insert(row)
+        for sent in sents:
+            self.sentdt.insert({'fkid':newid,'sent':sent})
+    
     
     
 if __name__ == '__main__':
@@ -31,31 +53,11 @@ if __name__ == '__main__':
     N = 5
     
     # insert one at a time
-    md = MyDocuments()
-    Nsent,sentlen = 50000,10
+    md = DocsWithSents('tmpdump.db',verbose=True)
+    Nsent,sentlen = 5,10
     for i in range(N):
         sents = [[str(i) for i in range(sentlen)] for _ in range(Nsent)]
-        md.insert({'name':'user_'+str(i), 'age':random.random(),'description':sents})
+        dat = {'name':'user_'+str(i), 'age':random.random()}
+        md.insert(dat,sents)
     print(md) # <DocTable2::mydocuments ct: 5>
     
-    print(len(md.select_first(md['description'])))
-    #print(md.select_first())
-    
-    # this will make a select statement to sents on every iteration so that
-    # the sql engine doesn't queue multiple objects into memory at once. This
-    # is less useful for sents unless there is a large number of sents per doc
-    # and memory is limited.
-    st = time()
-    print('running select_iter (one call per iter)')
-    for row in md.select_iter():
-        args = (row['name'],len(row['description']))
-        print('user {} has a description of {} sentences.'.format(*args))
-    print('took {} sec.\n'.format(time()-st))
-    
-    # makes a single call to teh subdoc table to load sents into memory
-    st = time()
-    print('running select')
-    for row in md.select():
-        args = (row['name'],len(row['description']))
-        print('user {} has a description of {} sentences.'.format(*args))
-    print('took {} sec.\n'.format(time()-st))
--- examples/dt2_schema.py
+++ examples/dt2_schema.py
@@ -6,76 +6,59 @@ import sys
 sys.path.append('..')
 from doctable import DocTable2
 
-class MyDocuments(DocTable2):
-    '''Class providing schema for MyDocuments database.
-    '''
+
+schema = (
+    # standard id column
+    #SQLAlchemy: Column('id', Integer, primary_key = True, autoincrement=True), 
+    ('id','integer',dict(primary_key=True, autoincrement=True)),
+
+    # make a category column with two options: "FICTION" and "NONFICTION"
+    #SQLAlchemy: Column('title', String,)
+    #SQLAlchemy: CheckConstraint('category in ("FICTION","NONFICTION")', name='salary_check')
+    ('category','string', dict(nullable=False)),
+    ('category in ("FICTION","NONFICTION")','check_constraint', dict(name='salary_check')),
+
+    # make a non-null title column
+    #SQLAlchemy: Column('title', String,)
+    ('title','string', dict(nullable=False)),
+
+    # make sure each category/title entry is unique
+    #SQLAlchemy:  UniqueConstraint('category', 'title', name='work_key')
+    (['category','title'],'unique_constraint',dict(name='work_key')),
+
+    # make an abstract where the default is an empty string instead of null
+    #SQLAlchemy: Column('abstract', String, default='')
+    ('abstract','string', dict(default='')),
+
+    # make an age column where age must be greater than zero
+    #SQLAlchemy: Column('abstract', Integer)
+    #SQLAlchemy: CheckConstraint('age > 0')
+    ('age','integer'),
+    ('age > 0','check_constraint'),
+
+    # make a column that keeps track of column updates
+    #SQLAlchemy: Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+    ('updated_on', 'datetime', dict(default=datetime.now, onupdate=datetime.now)),
+
+    # make a string column with max of 500 characters
+    #SQLAlchemy: Column('abstract', String, default='')
+    ('text','string',dict(),dict(length=500)),
     
-    # provide schema information
-    # each schema element can be a 
-    tabname = 'mydocuments'
-    schema = (
-        # standard id column
-        #SQLAlchemy: Column('id', Integer, primary_key = True, autoincrement=True), 
-        ('id','integer',dict(primary_key=True, autoincrement=True)),
-
-        # make a category column with two options: "FICTION" and "NONFICTION"
-        #SQLAlchemy: Column('title', String,)
-        #SQLAlchemy: CheckConstraint('category in ("FICTION","NONFICTION")', name='salary_check')
-        ('category','string', dict(nullable=False)),
-        ('category in ("FICTION","NONFICTION")','check_constraint', dict(name='salary_check')),
-
-        # make a non-null title column
-        #SQLAlchemy: Column('title', String,)
-        ('title','string', dict(nullable=False)),
-
-        # make sure each category/title entry is unique
-        #SQLAlchemy:  UniqueConstraint('category', 'title', name='work_key')
-        (['category','title'],'unique_constraint',dict(name='work_key')),
-
-        # make an abstract where the default is an empty string instead of null
-        #SQLAlchemy: Column('abstract', String, default='')
-        ('abstract','string', dict(default='')),
-
-        # make an age column where age must be greater than zero
-        #SQLAlchemy: Column('abstract', Integer)
-        #SQLAlchemy: CheckConstraint('age > 0')
-        ('age','integer'),
-        ('age > 0','check_constraint'),
-
-        # make a column that keeps track of column updates
-        #SQLAlchemy: Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
-        ('updated_on', 'datetime', dict(default=datetime.now, onupdate=datetime.now)),
-
-        # make a string column with max of 500 characters
-        #SQLAlchemy: Column('abstract', String, default='')
-        ('text','string',dict(),dict(length=500)),
-        
-        # make index table
-        # SQLAlchemy: Index('ind0', 'category', 'title', unique=True)
-        ('ind0', 'index', ('category','title'),dict(unique=True)),
-        
-        # make this id a foreign key to another table column (CAN'T SHOW HERE)
-        # SQLAlchemy: ForeignKeyConstraint(['id'], ['invoice.invoice_id',], onupdate="CASCADE", ondelete="CASCADE")
-        #((['id'],['othertablerefid']), 'foreignkey_constraint',dict(),dict(onupdate="CASCADE", ondelete="CASCADE")),
-        
-        # try out custom data types
-        # TokensType and ParagraphsType are defined in doctable/coltypes.py
-        # SQLAlchemy: Column('tokenized', TokensType), Column('sentencized', ParagraphsType)
-        ('sentencized','subdocs'),
-        ('tokenized','tokens'),
-
-    )
+    # make index table
+    # SQLAlchemy: Index('ind0', 'category', 'title', unique=True)
+    ('ind0', 'index', ('category','title'),dict(unique=True)),
     
-    def __init__(self, fname=':memory:', engine='sqlite', verbose=False):
-        '''Provides a simpler (or more complex) interface to the DocTable2 constructor.
-        '''
-        super().__init__(
-            self.schema, 
-            tabname=self.tabname, 
-            fname=fname,
-            engine=engine,
-            verbose=verbose,
-        )
+    # make this id a foreign key to another table column (CAN'T SHOW HERE)
+    # SQLAlchemy: ForeignKeyConstraint(['id'], ['invoice.invoice_id',], onupdate="CASCADE", ondelete="CASCADE")
+    #((['id'],['othertablerefid']), 'foreignkey_constraint',dict(),dict(onupdate="CASCADE", ondelete="CASCADE")),
+    
+    # try out custom data types
+    # TokensType and ParagraphsType are defined in doctable/coltypes.py
+    # SQLAlchemy: Column('tokenized', TokensType), Column('sentencized', ParagraphsType)
+    ('sentencized','subdocs'),
+    ('tokenized','tokens'),
+
+)
     
     
     
