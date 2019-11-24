2d59446f75176b9a6993593c4d0473952d215627
Devin J. Cornell <devinj.cornell@gmail.com>
2019-11-24 12:03:56 -0500

    First try at use_token and parse_token.

--- doctable/docparser.py
+++ doctable/docparser.py
@@ -2,32 +2,47 @@
 
 def DocParser:
     '''Class that maintains convenient functions for parsing Spacy doc objects.'''
-    def parse_tok(tok, capitalize_ents=True, replace_num='__NUM__', replace_digit='__DIGIT__', entity_map=dict()):
+    def parse_tok(tok, replace_num='__NUM__', replace_digit='__DIGIT__', lemmatize=False, other_convert=None, format_ents=True, ent_convert=None, other_convert=None):
         '''Convert spacy token object to string.
         Args:
             tok (spacy token or span): token object to convert to string.
-            capitalize_ents (bool): Replace whitespace with space and capitalize ents.
-            replace_num (bool): Replace number following tok.like_num (includes "five", 
+            replace_num (str/None): Replace number following tok.like_num (includes "five", 
                 or 5) with a special token (i.e. __NUM__). None means no replacement.
-            replace_digit (bool): Replace digit meeting tok.is_digit with special token. 
+            replace_digit (str/None): Replace digit meeting tok.is_digit with special token. 
                 Only used when replace_num is None.
-                
-            entity_map (dict): map from ent_type->function where will use
-                special conversion function to convert specific entity types.
+            lemmatize (bool): return lemma instead of full word.
+            other_convert (func): custom conversion function to happen as last step
+                for non-entities. This way can keep all other functionality.
+            format_ents (bool): Replace whitespace with space and capitalize first 
+                letter of ents.
+            ent_convert (func): custom conversion function to happen as last step
+                for entities. This way can keep all other functionality.
         '''
         
-        number_ents = ('NUMBER','MONEY','PERCENT','QUANTITY','CARDINAL','ORDINAL')
+        # replace characters
+        if replace_digit is not None and tok.is_digit:
+            return replace_digit
+        if replace_num is not None and tok.like_num:
+            return replace_num
         
         if tok.ent_type_ == '': # non-entity token
-            return tok.text.lower()
+            if lemmatize:
+                return tok.lemma_.lower().strip()
+            elif other_convert is not None:
+                return other_convert(tok)
+            else:
+                return tok.text.lower().strip()
         
         else: # token is entity
-        elif tok.ent_type_ in number_ents:
-            return tok.ent_type_
-        else:
-            return tok.text
+            if ent_convert is not None:
+                return ent_convert(tok)
+            elif format_ents:
+                return ' '.join([t.capitalize() for t in tok.text.split()])
+            else:
+                return tok.text.strip()
+        
 
-    def use_tok(tok, no_whitespace=True, no_punct=False, no_num=False, no_digit=True, no_stop=False):
+    def use_tok(tok, no_whitespace=True, no_punct=False, no_num=False, no_digit=False, no_stop=False, no_ent=False):
         '''Decide to use token or not (can be overridden).
         Args:
             no_whitespace (bool): exclude whitespace.
@@ -54,17 +69,21 @@ def DocParser:
             
         if no_digit:
             do_use_tok = do_use_tok and not tok.like_num
+        
+        if no_ent:
+            do_use_tok = do_use_tok and tok.ent_type_ != ''
             
         return do_use_tok
 
-    def parse_doc(doc, split_sents=True, merge_ents=False, use_tok_args=dict()):
+    def tokenize_doc(doc, split_sents=True, merge_ents=False, use_tok_args=dict()):
         '''Parse spacy doc object.
         Args:
             split_sents (bool): parse into list of sentence tokens using doc.sents.
             merge_ents (bool): merge multi_word entities into same
-            
+            use_tok_args (dict): arguments to be passed to .use_tok()
+            parse_tok_args (dict): arguments to pass to .parse_tok()
         '''
-            
+        
         if merge_ents:
             for ent in doc.ents:
                 ent.merge(tag=ent.root.tag_, ent_type=ent.root.ent_type_)
