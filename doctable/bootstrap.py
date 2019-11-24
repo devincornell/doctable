ee2b162fc3df6ed561245616c59d1754a3dc747d
Devin J. Cornell <devinj.cornell@gmail.com>
2019-11-24 10:46:08 -0500

    Added stateful sampling from bootstrap object.

--- doctable/bootstrap.py
+++ doctable/bootstrap.py
@@ -4,11 +4,29 @@ import numpy as np
 class DocBootstrap:
     def __init__(self, docs):
         self.docs = docs
+        self.ids = None
+    
+    def get_doc_sample(self, n):
+        '''Get list of bootstrapped sentences (doesn't make copy of bs sent ids).'''
+        ids = self.get_sample(n)
+        return [(idx, self.docs[idx]) for idx in ids]
+    
+    def get_sample(self, n):
+        '''Randomly sample ids.'''
+        return np.random.randint(len(self.docs), size=n)
         
-    def sample(self, n):
-        '''Yield bootstrapped sentences (doesn't make copy of bs sents).'''
-        ids = np.random.randint(len(self.docs), size=n)
-        for idx in ids:
-            yield self.docs[idx]
+    def draw_sample(self, n):
+        '''Set random sample in bootstrap object.'''
+        self.ids = self.get_sample(n)
+    
+    def get_docs(self):
+        '''Returns sents which are sampled from ids drawn in .set_sample()'''
+        if self.ids is None:
+            raise ValueError('Need to call .draw_sample() before getting docs. '
+                'Alternatively use .get_docs_single() to draw without setting sample.')
+        
+        return [(idx, self.docs[idx]) for idx in self.ids]
+        
+
     
 
