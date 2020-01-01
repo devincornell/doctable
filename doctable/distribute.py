65f0f3db79f28db343c36a081fa752eb219a627a
Devin J. Cornell <devinj.cornell@gmail.com>
2020-01-01 15:45:07 -0500

    Added features to distribute.

--- doctable/distribute.py
+++ doctable/distribute.py
@@ -25,13 +25,65 @@ class Distribute:
         
         if not self.finished:
             raise RuntimeError('There was a problem with the chunk_thread.')
-                
-    def chunkmap(self, elements, chunk_thread, *thread_args):
+    
+    
+    ########################### INSERT INTO DB ##########################
+    
+    def map_insert(self, thread_func, elements, *thread_args, dt_inst=None):
+        '''Map each element to be inserted in a doctable.
+        Description: Used primairly to maintain one doctable instance
+            per thread, but can have no other setup.
+        Args:
+            thread_func (func): applied to each element
+            elements (list<>): list of elements
+            thread_args: Additional args to be sent to each element
+        '''
+        was_connected = dt_inst is not None
+        had_conn = False
+        if was_connected:
+            had_conn = dt_inst._conn is not None
+            dt_inst.close_engine()
+        
+        results = self.map_chunk(self._map_insert_thread, 
+                    elements, thread_func, dt_inst, *thread_args)
+        
+        if was_connected:
+            dt_inst.open_engine(open_conn=had_conn)
+        
+        return results
+        
+    @staticmethod
+    def _map_insert_thread(el_chunk, thread_func, dt_inst, *static_args):
+        
+        # open connection to doctable
+        thread_args = list()
+        if dt_inst is not None:
+            dt_inst.open_engine(open_conn=True)
+            thread_args.insert(0, dt_inst)
+        
+        # map thread_func to each elements, passing the doctable instance
+        results = list()
+        for element in el_chunk:
+            results.append(thread_func(element, *thread_args, *static_args))
+            
+        if dt_inst is not None:
+            dt_inst.close_engine()
+        
+        return results
+    
+    
+    
+    ########################### CHUNK PROCESSING ##########################
+    def map_chunk(self, chunk_thread, elements, *thread_args):
+        '''Applies chunk_thread to chunks of elements.
+        '''
         # mark as in-progress
         self.finished = False
         
         # set number of workers as minimum
-        self.workers = min([len(elements), self.workers_og])        
+        self.workers = min([len(elements), self.workers_og])
+        if self.workers <= 1:
+            return chunk_thread(elements, *thread_args)
 
         # chunk up elements
         chunk_size = math.ceil(len(elements)/self.workers)
@@ -40,7 +92,7 @@ class Distribute:
         
         # create pool and pipes
         self.pipes = [Pipe(False) for i in range(self.workers)]
-        self.pool = [Process(target=self._distribute_chunks_thread_wrap, 
+        self.pool = [Process(target=self._map_chunk_thread, 
             args=(chunk_thread, self.pipes[i][1], chunks[i], thread_args)) 
             for i in range(self.workers)]
         
@@ -65,12 +117,14 @@ class Distribute:
         return results
         
     @staticmethod
-    def _distribute_chunks_thread_wrap(func, pipe, chunk, staticdata):
+    def _map_chunk_thread(func, pipe, chunk, staticdata):
+        # put some exception handling in here? This is a thread function
         res = func(chunk, *staticdata)
         if isinstance(res, list) or isinstance(res, tuple):
             pipe.send(res)
         else:
             pipe.send([res]*len(chunk))
+        
     
     
 #class DManager:
--- tests/test_distribte.py
+++ tests/test_distribte.py
@@ -6,11 +6,18 @@ import doctable
 def chunk_thread(elchunk):
     for i in elchunk:
         print(i)
+def thread_func(i):
+    print(i)
+    if i == 5:
+        raise KeyError()
 
 if __name__ == '__main__':
-    elements = list(range(100))
-    with doctable.Distribute() as d:
-        d.chunkmap(elements, chunk_thread)
+    elements = list(range(10))
+    with doctable.Distribute(2) as d:
+        d.map_chunk(chunk_thread, elements)
+        print('--------')
+        d.map_insert(thread_func, elements)
+        
     print('finished.')
     
     
