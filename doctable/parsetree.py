b39edd26d9f351ee22c5755fad999a442f2276d4
Devin J. Cornell <devinj.cornell@gmail.com>
2020-12-16 09:29:54 -0500

    made changes to indexing

--- doctable/parsetree.py
+++ doctable/parsetree.py
@@ -86,39 +86,50 @@ class Token:
     tag = None
     info = None
     
-    def __init__(self, node, tok_parse_func=None, info_func_map=dict(), parent=None):
-        '''Construct from either a dictionary or spacy token.'''
+    def __init__(self, node, text_parse_func=None, info_func_map=dict(), parent=None):
+        '''Construct from either a dictionary or spacy token.
+        Args:
+            if node is spacy.Token:
+                node (spacy.Token): token to extract info from
+                text_parse_func (dict<str:func>): mapping to store text
+                info_func_map (dict<str:func>): data that will go into .info
+            if node is dict:
+                node (dict<str:any>): dictionary of node properties
+                parent (spacy.Token): parent of current node
+                text_parse_func: unused
+                info_func_map: unused
+        '''
         self.parent = parent
         
-        if isinstance(node, dict):
+        if not isinstance(node, dict): # node is spacy token
+            tok = node
+            self.i = tok.i
+            self.text = text_parse_func(tok) if text_parse_func is not None else tok.text
+            self.dep = tok.dep_
+            self.tag = tok.tag_
+            self.info = {attr:func(tok) for attr,func in info_func_map.items()}
+            
+            self.childs = [self.__class__(child, text_parse_func=text_parse_func, \
+                            info_func_map=info_func_map, parent=self) 
+                           for child in tok.children]
+            
+            self._pos = tok.pos_ if tok.doc.is_tagged else None
+            self._ent = tok.ent_type_ if tok.doc.is_nered else None    
+
+        else: # node is a dictionary (probably re-creating token from stored dictionary)
             ndict = node
             self.i = ndict['i']
             self.text = ndict['text']
             self.dep = ndict['dep']
             self.tag = ndict['tag']
             self.info = ndict['info']
-            self.childs = [Token(c, parent=self) for c in ndict['childs']]
+            self.childs = [self.__class__(c, parent=self) for c in ndict['childs']]
             self._pos = ndict['pos']
             self._ent = ndict['ent']
 
-            
-        else: # node is spacy token
-            tok = node
-            self.i = tok.i
-            self.text = tok_parse_func(tok) if tok_parse_func is not None else tok.text
-            self.dep = tok.dep_
-            self.tag = tok.tag_
-            self.info = {attr:func(tok) for attr,func in info_func_map.items()}
-            
-            self.childs = [Token(child, tok_parse_func=tok_parse_func, \
-                            info_func_map=info_func_map, parent=self) 
-                           for child in tok.children]
-            
-            self._pos = tok.pos_ if tok.doc.is_tagged else None
-            self._ent = tok.ent_type_ if tok.doc.is_nered else None    
-        
     def to_dict(self):
-        '''Convert self to a dict.'''
+        '''Convert self to a dict - used when storing data.
+        '''
         node = dict(
             i=self.i,
             text=self.text,
@@ -133,13 +144,13 @@ class Token:
         
     ########################## Built-In Methods ##########################
     def __str__(self):
-        return 'Token({})'.format(self.text)
+        return f'{self.__class__.__name__}({self.text})'
     
     def __repr__(self):
         return str(self)
     
     def __getitem__(self,ind):
-        return self.childs[ind]
+        return self.info[ind]
     
     def __iter__(self):
         return iter(self.childs)
@@ -169,21 +180,28 @@ class Token:
 
     ########################## Navigation Functions ##########################
 
-    def get_deps(self, deprel):
+    def get_childs(self, dep=None, pos=None, matchfunc=None):
         ''' Get children with the given dependency relation.
         Args:
-            deprel (sequence or string): dependency relations to match on.
+            dep (sequence or string): dependency relations to match on.
+            pos (sequence or string): pos to match on.
+            matchfunc (function or None): custom matching function.
         '''
-        if isinstance(deprel, str):
-            deprel = set([deprel])
+        if isinstance(dep, str):
+            dep = set([dep])
+
+        if isinstance(pos, str):
+            pos = set([pos])
 
         childs = list()
         for c in self.childs:
-            if c.dep in deprel:
-                childs.append(c)
+            if (dep is None or c.dep in dep): # nested to make more readable?
+                if (pos is None or c.pos in pos):
+                    if (matchfunc is None or matchfunc(c)):
+                        childs.append(c)
         return childs
 
-    def get_dep(self, deprel, first=False):
+    def get_child(self, *args, first=False, **kwargs):
         ''' Get first child with the given dependency relation.
         Args:
             deprel (sequence or string): dependency relations to match on.
@@ -191,13 +209,13 @@ class Token:
             ValueError when the token has more than one dependency with the 
                 given relation.
         '''
-        deps = self.get_deps(deprel)
-        if len(deps) == 1 or (len(deps) and first):
-            return deps[0]
-        elif not len(deps):
+        childs = self.get_childs(*args, **kwargs)
+        if len(childs) == 1 or (len(childs) and first):
+            return childs[0]
+        elif not len(childs):
             return NoneToken()
         else:
-            raise ValueError(f'There is more than one dependency of types {deprel}.')
+            raise ValueError(f'There is more than one dependency matching {args}, {kwargs}.')
     
     def get_preps(self, as_str=False):
         ''' Gets chained prepositional phrases starting at the current token.
@@ -206,11 +224,11 @@ class Token:
         '''
         #if preps is None:
         preps = list()
-        for prep in self.get_deps({'prep', 'dative'}):
+        for prep in self.get_childs({'prep', 'dative'}):
             if as_str:
-                pairs = (prep.t, [p.t for p in prep.get_deps('pobj') if not p.is_none])
+                pairs = (prep.t, [p.t for p in prep.get_childs('pobj') if not p.is_none])
             else:
-                pairs = (prep, [p for p in prep.get_deps('pobj') if not p.is_none])
+                pairs = (prep, [p for p in prep.get_childs('pobj') if not p.is_none])
             preps.append(pairs)
                 
         return preps
