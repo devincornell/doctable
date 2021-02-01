dec18d39b041b9915863479bdf6ad3cb224a8aa6
Devin J. Cornell <devincornell@users.noreply.github.com>
2021-02-01 18:27:45 -0500

    Adding timer.

--- doctable/util.py
+++ doctable/util.py
@@ -12,3 +12,33 @@ def list_tables(target=':memory:', dialect='sqlite', **engine_args):
     engine = ConnectEngine(target=target, dialect=dialect, **engine_args)
     return engine.list_tables()
 
+class Timer:
+    """Times a task and outputs the elapsed time to screen and log file. 
+          Usage example:
+        : with Timer(name = "mytask") as t:
+        :     <do my task>
+    """
+    def __init__(self, name='running code', verbose=True):
+        self.name = name
+        self.verbose = verbose
+        self.message = '{name} took {delta}.'
+        if self.verbose:
+            print(f'{name}')
+    
+    def __enter__(self):
+        self.start = time.time()
+        return self
+        
+    def __exit__(self, *args):
+        self.delta = self.format_date(time.time() - self.start)
+        if self.verbose:
+            print(self.message.format(name=self.name, delta=self.delta))
+
+    def format_date(self, delta):
+        if delta > 60:
+            printstr = f'{delta/60:0.2f} minutes'
+        elif delta > 3600:
+            printstr = f'{delta/3600:0.2f} hours'
+        else:
+            printstr = f'{delta:0.2f} seconds'
+        return printstr
