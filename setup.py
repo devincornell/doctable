6fb5e39d9cb53f20a3129b1b8ac1af32c19e7d00
Devin Cornell <whatever@screwgit.com>
2021-03-08 22:30:26 -0500

    upgrading version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.8.7'
+version = '0.8.8'
 setup(name='doctable',
     version='{}'.format(version),
     description='Simple database interface for text analysis applications.',
