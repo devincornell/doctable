eccd070e8928ae8fcfff21a45b03a82637f277fd
Devin Cornell <whatever@screwgit.com>
2021-03-13 13:16:11 -0500

    updating version that handles dataclass schemas

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.8.8'
+version = '0.8.9'
 setup(name='doctable',
     version='{}'.format(version),
     description='Simple database interface for text analysis applications.',
