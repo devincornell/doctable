376ca7d0c65bfa0b0bcabf26b14c324c45797b83
Devin Cornell <whatever@screwgit.com>
2021-04-10 16:14:37 -0400

    first try at allowing specific column types.

--- doctable/schemas/parse_schema_dataclass.py
+++ doctable/schemas/parse_schema_dataclass.py
@@ -14,25 +14,31 @@ def parse_schema_dataclass(dclass):
     # regular data columns (uses dataclass features)
     for f in fields(dclass):
         if f.init:
-            use_type = python_to_slqlchemy_type.get(f.type, sa.PickleType)
+            if 'coltype' in f.metadata: # specified type using string
+                use_type = string_to_sqlalchemy_type[f.metadata['coltype']]
+                del f.metadata['coltype']
+            
+            else: # infer column type based on python datatype hints
+                use_type = python_to_slqlchemy_type.get(f.type, sa.PickleType)
+            
             col = sa.Column(f.name, use_type, **f.metadata)
             columns.append(col)
 
-    #__indices__ = {
+    #_indices_ = {
     #    'my_index': ('c1', 'c2', {'unique':True}),
     #    'other_index': ('c1',),
     #}
-    if hasattr(dclass, '__indices__') and dclass.__indices__ is not None:
-        for name, vals in dclass.__indices__.items():
+    if hasattr(dclass, '_indices_') and dclass._indices_ is not None:
+        for name, vals in dclass._indices_.items():
             args, kwargs = get_kwargs(vals)
             columns.append(sa.Index(name, *args, **kwargs))
 
-    #__constraints__ = (
+    #_constraints_ = (
     #    ('check', 'x > 3', dict(name='salary_check')), 
     #    ('foreignkey', ('a','b'), ('c','d'))
     #)
-    if hasattr(dclass, '__constraints__') and dclass.__constraints__ is not None:
-        for vals in dclass.__constraints__:
+    if hasattr(dclass, '_constraints_') and dclass._constraints_ is not None:
+        for vals in dclass._constraints_:
             args, kwargs = get_kwargs(vals)
             columns.append(constraint_lookup[args[0]](*args[1:], **kwargs))
 
