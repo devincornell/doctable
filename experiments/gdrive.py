f0a9e56b584d7c1ad2bc25838f18f3628e30916b
Devin Cornell <whatever@screwgit.com>
2021-05-02 14:29:51 -0400

    moved gdrive tests

--- experiments/gdrive.py
+++ experiments/gdrive.py
@@ -1,11 +1,1 @@
 
-import sys
-sys.path.append('..')
-import doctable
-
-
-if __name__ == '__main__':
-    drive = doctable.Drive('credentials/client_secret_4175.json')
-    drive.print_files('1NHEvd4auC4jmFZbh-KUPsicYs6zVcGRf')
-    drive.upload_file('tmp.txt', '1NHEvd4auC4jmFZbh-KUPsicYs6zVcGRf')
-
