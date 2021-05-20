266faf54fdd28ef2b40d67b02cbf685aa1449540
devincornell <devincornell@example.com>
2021-05-20 15:00:56 -0400

    upgrading version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.9.0'
+version = '0.9.1'
 setup(name='doctable',
     version='{}'.format(version),
     description='Simple database interface for text analysis applications.',
