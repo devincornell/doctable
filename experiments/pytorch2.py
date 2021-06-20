a37b3d1412e39645c55931ec356c98e07c606095
devincornell <devincornell@example.com>
2021-06-20 08:16:45 -0400

    added benchmarks for chunked multiplications

--- experiments/pytorch2.py
+++ experiments/pytorch2.py
@@ -1,6 +1,7 @@
 
 import torch
 from tqdm import tqdm
+import numpy as np
 
 import sys
 sys.path.append('..')
@@ -8,12 +9,12 @@ import doctable
 
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     device = torch.device('cuda:0')
     #device = torch.device('cpu')
 
     # TASK: find sims between all points in this space
-    shape = (int(2.5e6), 300)
+    shape = (int(2.5e5), 300)
     X = torch.rand(shape).to(device)
     X = X / X.norm(p=2, dim=1)[:,None] # row-normalize
     #print(X.norm(p=2, dim=1))
@@ -22,7 +23,14 @@ if __name__ == '__main__':
     for s in tqdm(doctable.chunk_slice(X.shape[0], chunk_size=100)):
         sims[s] = X.matmul(X[s,:].T).T.mean(1)
 
-    print((sims==0).sum())
+    lsims = list()
+    for i in tqdm(range(X.shape[0])):
+        lsims.append(X.mv(X[i,:]).mean().cpu())
+    lsims = np.array(lsims)
+
+    print(sims.shape, lsims.shape)
+    print(sims[:5])
+    print(lsims[:5])
         
 
 
