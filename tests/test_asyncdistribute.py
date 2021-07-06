06606918539eefbbb5ccaa995c25c2d669a28e65
devincornell <devincornell@example.com>
2021-07-06 10:27:28 -0400

    fixed test

--- tests/test_asyncdistribute.py
+++ tests/test_asyncdistribute.py
@@ -7,7 +7,7 @@ import random
 
 import multiprocessing
 
-def test_func(x):
+def example_func(x):
     return x**2
 
 def test_asyncdistribute(n=100):
@@ -18,9 +18,9 @@ def test_asyncdistribute(n=100):
     
 
     with doctable.AsyncDistribute(2) as d:
-        test_output = d.map(test_func, test_input)
+        test_output = d.map(example_func, test_input)
 
-    assert(test_output == list(map(test_func, test_input)))
+    assert(test_output == list(map(example_func, test_input)))
 
 if __name__ == '__main__':
     test_asyncdistribute()
