3b27404a84dcde77b974355c198a2c29da553515
devincornell <devincornell@example.com>
2022-09-10 12:23:19 -0400

    adding the experiment I used to check types for select queries

---
+++ experiments/experiment_typing.py
@@ -0,0 +1,29 @@
+from typing import Union, Mapping, Sequence, Tuple, Set, List
+from dataclasses import dataclass#, field, fields
+import datetime
+import pandas as pd
+
+import sqlalchemy
+
+import sys
+sys.path.append('..')
+import doctable
+
+@doctable.schema(require_slots=False)
+class MySchema:
+    _id: int = doctable.IDCol()
+    name: str = doctable.Col()
+
+if __name__ == '__main__':
+    
+    tab = doctable.DocTable(target=':memory:', schema=MySchema)
+    tab.insert_single({'name': 'Devin'})
+    tab.insert_single({'name': 'Devin'})
+    tab.insert_single({'name': 'Carl'})
+
+    print(type(tab.c.name.desc()))
+    print(type(tab.c.name))
+    
+    #orderby=tab['name'].desc()
+    print(type(sqlalchemy.sql.select(tab.columns)))
+    print(tab.select())
