3a9f3788ca0ec7eb17e2b7986a3fdcc252574b46
Devin J. Cornell <devincornell@github.com>
2023-02-04 14:59:50 -0500

    updated assertions

--- doctable/util/asserts.py
+++ doctable/util/asserts.py
@@ -1,23 +1,33 @@
 import typing
 
+T = typing.TypeVar('T')
 
-def assert_set_len_eq(x: typing.Any, y: typing.Any) -> None:
+def assert_set_len_eq(x: T, y: T) -> None:
     f = lambda x: len(set(x))
-    try:
-        assert(f(x)) == f(y)
-    except AssertionError as e:
-        raise AssertionError(f'Failed: {len(set(x))=} != {len(set(y))=}. '
+    if f(x) == f(y):
+        raise AssertionError(f'Failed assertion: {len(set(x))=} != {len(set(y))=}. '
         f'The sets were different sizes.')
 
-def assert_set_eq(x: typing.Any, y: typing.Any) -> None:
-    try:
-        assert(set(x)) == set(y)
-    except AssertionError as e:
-        raise AssertionError(f'Failed: set({len(set(x))=}) != set({len(set(y))=})')
+def assert_set_eq(x: T, y: T) -> None:
+    if set(x) != set(y):
+        raise AssertionError(f'Failed assertion: set({len(set(x))=}) != set({len(set(y))=})')
 
-def assert_eq(x: typing.Any, y: typing.Any) -> None:
-    try:
-        assert(x == y)
-    except AssertionError as e:
-        raise AssertionError(f'Failed: x != y:\n{x=}\n{y=}')
-    
+def assert_eq(x: T, y: T) -> None:
+    if x != y:
+        raise AssertionError(f'Failed assertion x == y:\n{x=}\n{y=}')
+
+def assert_neq(x: T, y: T) -> None:
+    if x == y:
+        raise AssertionError(f'Failed assertion x != y:\n{x=}\n{y=}')
+
+def assert_ge(x: T, y: T) -> None:
+    if x < y:
+        raise AssertionError(f'Failed assertion x >= y:\n{x=}\n{y=}')
+
+def assert_gt(x: T, y: T) -> None:
+    if x <= y:
+        raise AssertionError(f'Failed assertion: x > y:\n{x=}\n{y=}')
+
+def assert_eq_transform(x: T, y: T, f: typing.Callable[[T],typing.Any]):
+    if f(x) != f(y):
+        raise AssertionError(f'Failed assertion: {f(x)=} != f({f(y)=}).')
