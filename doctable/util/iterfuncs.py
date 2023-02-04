075ae447ca147ec4de682ff41276aa2a15455661
Devin J. Cornell <devincornell@github.com>
2023-02-04 14:59:59 -0500

    finished functions used for grouping

--- doctable/util/iterfuncs.py
+++ doctable/util/iterfuncs.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 
 import typing
 
@@ -5,29 +6,82 @@ import typing
 T = typing.TypeVar('T')
 
 
-def ungroupby(groups: typing.Dict[typing.Hashable, T]):
-    '''Flatten any list'''
-    pass
+class GroupedData(typing.Dict[typing.Hashable, typing.Union[typing.List, typing.Dict]]):
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({super().__repr__()})'
+    
+    ################### Grouping ###################
+    @classmethod
+    def group_elements(cls, 
+            elements: typing.Iterable[T], 
+            keys: typing.List[typing.Callable[[T], typing.Hashable]],
+        ) -> GroupedData:
+        '''Groups elements of the list into nested dictionaries according to group funcs.'''
+        
+        groups = cls()
+        num_keys = len(keys)
+        for el in elements:
+            # down each level of the heirarchy
+            grp = groups
+            for i, key_func in enumerate(keys):
+                k = key_func(el)
+                if i < (num_keys - 1):
+                    grp.setdefault(k, cls())
+                    grp = grp[k]
+                else:
+                    grp.setdefault(k, list())
+                    grp[k].append(el)
+        return groups
+    
+    ################### Flattening ###################
+    def ungroup(self) -> typing.List[typing.Tuple[typing.Tuple[typing.Hashable], T]]:
+        elements = list()
+        for keys, els in self.flatten().items():
+            for e in els:
+                elements.append((keys, e))
+        return elements
+    
+    
+    def flatten(self) -> GroupedData:
+        groups = self.__class__()
+        for k,els in self.items():
+            if hasattr(els, 'flatten'):
+                for ks, e in els.flatten().items():
+                    groups[(k,)+ks] = e
+            else:
+                groups[(k,)] = els
+        return groups
+
+        
+    
+    def ungroup_old(self) -> typing.List[typing.Tuple[typing.Tuple[typing.Hashable], T]]:
+        elements = list()
+        for k,els in self.items():
+            if hasattr(els, 'ungroup_recursive'):
+                for ks, e in els.ungroup_recursive():
+                    print(k, ks)
+                    elements.append(((k,)+ks, e))
+            else:
+                for e in els:
+                    elements.append(((k,), e))
+        return elements
+    
+    def ungroup_recursive(self) -> typing.List[typing.Tuple[typing.List[typing.Hashable], T]]:
+        elements = list()
+        for k,els in self.items():
+            if hasattr(els, 'ungroup_recursive'):
+                for ks, e in els.ungroup_recursive():
+                    elements.append(([k]+ks, e))
+            else:
+                for e in els:
+                    elements.append(([k], e))
+        return elements
 
 def groupby(
         elements: typing.Iterable[T], 
         keys: typing.List[typing.Callable[[T], typing.Hashable]],
-    ) -> typing.Dict[typing.Hashable, typing.Dict[typing.Hashable, typing.List[T]]]:
+    ) -> GroupedData:
     '''Groups elements of the list into nested dictionaries according to group funcs.'''
-    
-    groups = dict()
-    num_keys = len(keys)
-    for el in elements:
-        # down each level of the heirarchy
-        grp = groups
-        for i, key_func in enumerate(keys):
-            k = key_func(el)
-            if i < (num_keys - 1):
-                grp.setdefault(k, dict())
-                grp = grp[k]
-            else:
-                grp.setdefault(k, list())
-                grp[k].append(el)
-    return groups
-        
+    return GroupedData.group_elements(elements, keys)
 
--- tests/test_iterfuncs.py
+++ tests/test_iterfuncs.py
@@ -17,15 +17,28 @@ class Element:
 def test_groupby():
     
     elements = [Element(f'name_{i//3}', i, i*10) for i in range(10)]
+
+    groups = doctable.groupby(elements, keys=[lambda e: e.a])
+    pprint.pprint(groups)
+    assert(len(groups)==4)
+    pprint.pprint(groups.flatten())
+    pprint.pprint(groups.ungroup())
+
         
     groups = doctable.groupby(elements, keys=[lambda e: e.a, lambda e: e.b % 2, lambda e: e.c//20])
     pprint.pprint(groups)
     assert(len(groups) == 4)
     assert(len(groups['name_0']) == 2)
-
-    groups = doctable.groupby(elements, keys=[lambda e: e.a])
+    pprint.pprint(groups.flatten())
+    pprint.pprint(groups.ungroup())
+    
+    keys = [lambda e: e.a, lambda e: e.b % 2, lambda e: e.c//20]
+    groups = doctable.GroupedData.group_elements(elements, keys=keys)
     pprint.pprint(groups)
     assert(len(groups)==4)
+    pprint.pprint(groups.ungroup())
+    pprint.pprint(groups.flatten())
+
 
 if __name__ == '__main__':
     test_groupby()
