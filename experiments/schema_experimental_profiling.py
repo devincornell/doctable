0b105ae24fc71f53183358f1d9a598ef70bf0a97
Devin J. Cornell <devincornell@github.com>
2023-02-12 12:22:14 -0500

    built profile for experimental schema

---
+++ experiments/schema_experimental_profiling.py
@@ -0,0 +1,76 @@
+import sys
+sys.path.append('..')
+import doctable
+import dataclasses
+import functools
+import tempfile
+import random
+import copy
+import typing
+import cProfile
+
+@doctable.schema
+class MyObjSmall:
+    __slots__ = []
+    id: int = doctable.Col()
+    name: str = doctable.Col()
+    other: float = doctable.Col()
+    other2: float = doctable.Col()
+
+@doctable.schema_experimental
+class ExMyObjSmall:
+    id: int = doctable.ExpIDCol()
+    name: str = doctable.ExpCol()
+    other: float = doctable.Col()
+    other2: float = doctable.Col()
+
+def make_test_dt(SchemaClass, in_memory: bool = True, tmpdir: str = None) -> doctable.DocTable:
+    dt_small = doctable.DocTable(
+        schema=SchemaClass,
+        target = ':memory:' if in_memory else f'{tmpdir}/{SchemaClass}_test.db',
+        new_db = True,
+    )
+    return dt_small
+
+def test_insert_and_select(test_objs: typing.List[MyObjSmall], table: doctable.DocTable):
+    with table as tab:
+        tab.q.insert_multi(test_objs)
+        a = tab.q.select()
+
+def runtest_exp(n: int = 100):
+    print(f'=== start EXPERIMENTAL test: {n=} ================')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dt_small = make_test_dt(ExMyObjSmall, tmpdir)
+        print(f'construct {n} objects')
+        [ExMyObjSmall(i, f'name_{i}') for i in range(n)]
+        #[ExMyObjSmall.from_dict({'id': i, 'name': f'name_{i}'}) for i in range(n)]
+        test_objs = [ExMyObjSmall(i, f'name_{i}') for i in range(n)]
+
+def runtest_obj(n: int = 100):
+    print(f'=== start OBJECT test: {n=} ================')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dt_small = make_test_dt(MyObjSmall, tmpdir)
+        print(f'construct {n} objects')
+        test_objs = [MyObjSmall(i, f'name_{i}') for i in range(n)]
+
+def runtest_raw(n: int = 100):
+    print(f'=== start RAW test: {n=} ================')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dt_small = make_test_dt(MyObjSmall, tmpdir)
+        print(f'construct {n} objects')
+        test_objs = [{'id': i, 'name': f'name_{i}'} for i in range(n)]
+
+def test_create_objects(ObjType: type, payload: typing.List[typing.Tuple[int,str]]):
+    for p in payload:
+        ObjType(*p)
+
+# these can't be in __main__
+payload = [(i, f'name{i}', i * 1.1, i * 2.1) for i in range(100000)]
+cProfile.run('test_create_objects(MyObjSmall, payload)', filename='bench/schema_obj_create.prof')
+cProfile.run('test_create_objects(ExMyObjSmall, payload)', filename='bench/schema_experimental_obj_create.prof')
+
+if __name__ == '__main__':
+    
+    pass
+
+
