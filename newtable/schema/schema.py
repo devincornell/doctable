a515bb0a2ecc0b5a4c555bfb592591c2855dd18c
Devin J. Cornell <devincornell@github.com>
2023-10-10 08:39:36 -0400

    making infrastructure to support schema creation

--- newtable/schema/schema.py
+++ newtable/schema/schema.py
@@ -6,23 +6,20 @@ import sqlalchemy
 
 from .column import ColumnInfo
 
+def index(*column_names: typing.List[str], **kwargs: typing.Dict[str, typing.Any]) -> IndexInfo:
+    return IndexInfo(
+        column_names=column_names,
+        kwargs=kwargs,
+    )
+
 @dataclasses.dataclass
 class IndexInfo:
     column_names: typing.List[str]
-
-    @classmethod
-    def new(cls, *column_names) -> IndexInfo:
-        '''Get index info from only a sqlalchemy type.'''
-        return cls(column_names=list(column_names))
-
-    @classmethod
-    def from_field(cls, field: dataclasses.Field) -> IndexInfo:
-        '''Get index info from a dataclass field.'''
-        pass
+    kwargs: typing.Dict[str, typing.Any]
 
     def sqlalchemy_index(self, name: str) -> sqlalchemy.Index:
-        return sqlalchemy.Index(name, *self.column_names)
-    
+        return sqlalchemy.Index(name, *self.column_names, **self.kwargs)
+
 
 T = typing.TypeVar('T')
 
@@ -35,17 +32,7 @@ class Schema(typing.Generic[T]):
     constraints: typing.List[sqlalchemy.Constraint]
     table_kwargs: typing.Dict[str, typing.Any]
 
-    def sqlalchemy_indices(self) -> typing.List[sqlalchemy.Index]:
-        '''Get list of index info.'''
-        return list(self.indices.values())
-
-    def sqlalchemy_columns(self) -> typing.List[sqlalchemy.Column]:
-        '''Get column objects from .'''
-        columns = list()
-        for field in dataclasses.fields(self.container_type):
-            columns.append(ColumnInfo.from_field(field))
-        return columns
-
+    #################### Converting to/from Container Types ####################
     def container_from_row(self, row: sqlalchemy.Row) -> T:
         '''Get a data container from a row.'''
         return self.container_type(**row._mapping)
@@ -54,15 +41,20 @@ class Schema(typing.Generic[T]):
         '''Get a dictionary representation of this schema.'''
         return dataclasses.asdict(container)
 
+    #################### Table Arguments ####################
     def table_args(self) -> typing.List[typing.Union[sqlalchemy.Column, sqlalchemy.Index, sqlalchemy.Constraint]]:
         '''Get a list of all table args.'''
         return [*self.sqlalchemy_columns(), *self.sqlalchemy_indices(), *self.constraints]
-    
-    def sqlalchemy_columns(self) -> typing.List[sqlalchemy.Column]:
-        '''Get list of sqlalchemy columns.'''
-        return [col.sqlalchemy_column(name) for name, col in self.columns.items()]
 
     def sqlalchemy_indices(self) -> typing.List[sqlalchemy.Index]:
         '''Get list of sqlalchemy indices.'''
         return [ind.sqlalchemy_index(name) for name, ind in self.indices.items()]
+
+    def sqlalchemy_columns(self) -> typing.List[sqlalchemy.Column]:
+        '''Get column objects from .'''
+        columns = list()
+        for field in dataclasses.fields(self.container_type):
+            columns.append(ColumnInfo.from_field(field))
+        return columns
+
 
