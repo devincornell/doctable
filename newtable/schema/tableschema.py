6f5075c9de9455606485e930b398073a7ede6c40
Devin J. Cornell <devincornell@github.com>
2023-10-11 09:18:26 -0400

    finished first draft of schema decorator

--- newtable/schema/tableschema.py
+++ newtable/schema/tableschema.py
@@ -3,94 +3,68 @@ from __future__ import annotations
 import typing
 import dataclasses
 import sqlalchemy
+import functools
 
 from .column import ColumnInfo, ColumnParams
 from .index import IndexInfo, IndexParams
 
-def schema(
-        _Cls: type = None, 
-        **table_kwargs: typing.Dict[str, typing.Any]
-    ) -> type:
-    '''A decorator to change a regular class into a schema object class.
-    '''
-    # in case the user needs to access the old version
-    if enable_properties:
-        decorator_schema = schema_decorator_properties_factory(
-            require_slots=require_slots, 
-            dataclass_kwargs=dataclass_kwargs
-        )
-    else:
-        decorator_schema = schema_decorator_basic_factory(
-            require_slots=require_slots, 
-            dataclass_kwargs=dataclass_kwargs
-        )
-
-    if _Cls is None:
-        return decorator_schema
-    else:
-        return decorator_schema(_Cls)
-
-def schema_decorator_properties_factory(
-        require_slots: bool, 
-        dataclass_kwargs: typing.Dict[str, typing.Any]
-    ) -> typing.Callable[[type], type]:
-
-    # this is the actual decorator
-    def schema_decorator_with_properties(Cls):
-        # creates constructor/other methods using dataclasses
-        Cls = dataclasses.dataclass(Cls, **dataclass_kwargs)
-        
-        if require_slots and not hasattr(Cls, '__slots__'):
-            raise SlotsRequiredError('Slots must be enabled by including "__slots__ = []". '
-                'Otherwise set doctable.schema(require_slots=False).')
-        
-        attr_map = dict()
-        for field in dataclasses.fields(Cls):
-            attr_name = property_to_attr(field.name)#f'_{field.name}'            
-            attr_map[field.name] = attr_name
-            
-            # dataclasses don't actually create the property unless the default
-            # value was a constant, so we just want to replicate that behavior
-            # with MISSING_VALUE as the object. Normally it would do that in the 
-            # constructor, but we want to create it ahead of time.
-            if hasattr(Cls, field.name):
-                setattr(Cls, attr_name, getattr(Cls, field.name))
-            else:
-                setattr(Cls, attr_name, MISSING_VALUE)
-            
-            # used a function to generate a class and return the property
-            # to solve issue with class definitions in loops
-            setattr(Cls, field.name, get_getter_setter(attr_name))
-        
-        # set map from properties to attributes
-        set_attr_map(Cls, attr_map)
-                
-        # implement new hash function if needed
-        if hasattr(Cls, '__hash__'):
-            def hashfunc(self):
-                return hash(as_value_tuple(self))
-            Cls.__hash__ = hashfunc
-            
-        def binary_compare_factory(attrname: str):
-            def binary_compare(self: Cls, other: Cls):
-                return getattr(as_value_tuple(self), attrname)(as_value_tuple(other))
-        
-        if hasattr(Cls, '__eq__'): Cls.__eq__ = binary_compare_factory('__eq__')
-        if hasattr(Cls, '__lt__'): Cls.__lt__ = binary_compare_factory('__lt__')
-        if hasattr(Cls, '__le__'): Cls.__le__ = binary_compare_factory('__le__')
-        if hasattr(Cls, '__gt__'): Cls.__gt__ = binary_compare_factory('__gt__')
-        if hasattr(Cls, '__ge__'): Cls.__ge__ = binary_compare_factory('__ge__')
-        
-        @functools.wraps(Cls, updated=[])
-        class NewClass(DocTableSchema, Cls):
-            __slots__ = tuple(an for pn,an,v in attr_value_tuples(Cls))
-            
-        return NewClass
-    
-    return schema_decorator_with_properties
-
+SCHEMA_ATTRIBUTE_NAME = '_table_schema'
 
 T = typing.TypeVar('T')
+
+def table_schema(
+        _Cls: typing.Type[T] = None, 
+        table_name: typing.Optional[str] = None,
+        indices: typing.Optional[typing.Dict[str, IndexParams]] = None,
+        constraints: typing.Optional[typing.List[sqlalchemy.Constraint]] = None,
+        init: bool = True, # these are all dataclass arguments (superset from 3.12)
+        repr: bool = True, # passed to dataclasses.dataclass()
+        eq: bool = True, # passed to dataclasses.dataclass()
+        order: bool = False, # passed to dataclasses.dataclass()
+        unsafe_hash: bool = False, # passed to dataclasses.dataclass()
+        frozen: bool = False, # passed to dataclasses.dataclass()
+        match_args: bool = True, # passed to dataclasses.dataclass()
+        kw_only: bool = False, # passed to dataclasses.dataclass()
+        slots: bool = False, # passed to dataclasses.dataclass()
+        weakref_slot: bool = True, # passed to dataclasses.dataclass()
+        **table_kwargs: typing.Dict[str, typing.Any],
+    ) -> typing.Type[T]:
+    '''A decorator to change a regular class into a schema object class.
+    '''
+    # handle case with no indices or constraints
+    indices = indices if indices is not None else dict()
+    constraints = constraints if constraints is not None else list()
+
+    # in case the user needs to access the old version
+    def table_schema_decorator(Cls: typing.Type[T]):
+        # creates constructor/other methods using dataclasses
+        Cls: typing.Type[T] = dataclasses.dataclass(
+            _Cls = Cls,
+            init = init,
+            repr = repr,
+            eq = eq,
+            order = order,
+            unsafe_hash = unsafe_hash,
+            frozen = frozen,
+            match_args = match_args,
+            kw_only = kw_only,
+            slots = slots,
+            weakref_slot = weakref_slot,
+        )
+        setattr(Cls, SCHEMA_ATTRIBUTE_NAME, TableSchema.from_container(
+            table_name  = table_name,
+            container_type = Cls,
+            indices = indices,
+            constraints = constraints,
+            table_kwargs = table_kwargs,
+        ))
+
+        return Cls
+                    
+    if _Cls is None:
+        return table_schema_decorator
+    else:
+        return table_schema_decorator(_Cls)
 
 @dataclasses.dataclass
 class TableSchema(typing.Generic[T]):
