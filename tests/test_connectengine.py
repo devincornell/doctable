14e80945f108e7bfb2df70fa8ab085cbff53eb1a
Devin J. Cornell <devincornell@github.com>
2023-11-02 11:26:48 -0400

    changed "newtable" to doctable

--- tests/test_connectengine.py
+++ tests/test_connectengine.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import sys
 sys.path.append('..')
-import newtable
+import doctable
 import sqlalchemy
 
 import dataclasses
@@ -13,30 +13,30 @@ class TestDataClass:
     age: int = None
 
 
-def dummy_container(table_name: str = 'test') -> newtable.Container:
-    @newtable.table_schema(table_name=table_name)
+def dummy_container(table_name: str = 'test') -> doctable.Container:
+    @doctable.table_schema(table_name=table_name)
     class TestContainer:
         name: str
         age: int
-        id: int = newtable.Column(column_args=newtable.ColumnArgs(order=0, primary_key=True, autoincrement=True))
+        id: int = doctable.Column(column_args=doctable.ColumnArgs(order=0, primary_key=True, autoincrement=True))
 
     return TestContainer
 
 
 def dummy_container2(table_name: str = 'test'):
     
-    @newtable.table_schema(
+    @doctable.table_schema(
         table_name=table_name,
-        indices = {f'{table_name}_age_name_index': newtable.Index('age', 'name')},
+        indices = {f'{table_name}_age_name_index': doctable.Index('age', 'name')},
     )
     class DummyContainer:
         name: str
         age: int
-        id: int = newtable.Column(
-            column_args=newtable.ColumnArgs(order=0, primary_key=True, autoincrement=True),
+        id: int = doctable.Column(
+            column_args=doctable.ColumnArgs(order=0, primary_key=True, autoincrement=True),
         )
-        updated: datetime.datetime = newtable.Column(
-            column_args=newtable.ColumnArgs(
+        updated: datetime.datetime = doctable.Column(
+            column_args=doctable.ColumnArgs(
                 default=datetime.datetime.utcnow, 
                 onupdate=datetime.datetime.utcnow
             )
@@ -45,16 +45,16 @@ def dummy_container2(table_name: str = 'test'):
     return DummyContainer
 
 def dummy_schema(table_name: str = 'test'):    
-    return newtable.TableSchema(
+    return doctable.TableSchema(
         table_name=table_name,
         container_type = TestDataClass,
         columns = [
-            newtable.ColumnInfo.default('id', int),
-            newtable.ColumnInfo.default('name', str),
-            newtable.ColumnInfo.default('age', int),
+            doctable.ColumnInfo.default('id', int),
+            doctable.ColumnInfo.default('name', str),
+            doctable.ColumnInfo.default('age', int),
         ],
         indices = [
-            newtable.IndexInfo(f'{table_name}_age_index', params=newtable.IndexParams.default('age')),
+            doctable.IndexInfo(f'{table_name}_age_index', params=doctable.IndexParams.default('age')),
         ],
         constraints = [],
         table_kwargs = {},
@@ -68,7 +68,7 @@ def test_new_connectengine(test_fname: str = 'test.db'):
         
     # can't open a non-existent database using open_existing
     try:
-        newtable.ConnectCore.open_existing(
+        doctable.ConnectCore.open_existing(
             target=test_fname, 
             dialect='sqlite',
         )
@@ -77,7 +77,7 @@ def test_new_connectengine(test_fname: str = 'test.db'):
         print(e)
     
     # create a new database from scratch
-    ce = newtable.ConnectCore.open_new(
+    ce = doctable.ConnectCore.open_new(
         target = test_fname, 
         dialect='sqlite',
         echo = True,
@@ -85,7 +85,7 @@ def test_new_connectengine(test_fname: str = 'test.db'):
     
 
 def test_sqlalchemy_table(test_table: str = 'test'):
-    ce = newtable.ConnectCore.open_new(
+    ce = doctable.ConnectCore.open_new(
         target = ':memory:', 
         dialect='sqlite',
     )
@@ -95,7 +95,7 @@ def test_sqlalchemy_table(test_table: str = 'test'):
     # can't reflect from a non-existent table
     try: # NOTE: database is actually created here
         tab0 = ce.reflect_sqlalchemy_table(table_name=test_table)
-    except newtable.TableDoesNotExistError as e:
+    except doctable.TableDoesNotExistError as e:
         print(e)
     #return ce
 
@@ -123,7 +123,7 @@ def test_sqlalchemy_table(test_table: str = 'test'):
         )
         print(tab2)
         raise Exception('Should have raised TableAlreadyExistsError.')
-    except newtable.TableAlreadyExistsError as e:
+    except doctable.TableAlreadyExistsError as e:
         print(e)
 
     # doesn't raise exception because extend_existing=True
@@ -154,18 +154,18 @@ def test_sqlalchemy_table(test_table: str = 'test'):
 
 
 def test_new_doctable(test_table: str = 'test'):
-    ce = newtable.ConnectCore.open_new(
+    ce = doctable.ConnectCore.open_new(
         target = ':memory:', 
         dialect='sqlite',
     )
 
     try:
-        newtable.ReflectedDBTable.from_existing_table(test_table, core=ce)
+        doctable.ReflectedDBTable.from_existing_table(test_table, core=ce)
         raise Exception('Should have raised TableDoesNotExistError.')
-    except newtable.TableDoesNotExistError as e:
+    except doctable.TableDoesNotExistError as e:
         print(e)
 
-    #t = newtable.DocTable.from_schema(dummy_schema(), core=ce)
+    #t = doctable.DocTable.from_schema(dummy_schema(), core=ce)
     container = dummy_container(test_table)
     print(dir(container), container)
     #with ce.emit_ddl() as emitter:
@@ -188,7 +188,7 @@ def test_new_doctable(test_table: str = 'test'):
     print(ce.inspect_columns(test_table))
     print(ce.inspect_indices(test_table))
 
-    ce = newtable.ConnectCore.open_new(
+    ce = doctable.ConnectCore.open_new(
         target = ':memory:', 
         dialect='sqlite',
     )
@@ -196,8 +196,8 @@ def test_new_doctable(test_table: str = 'test'):
 
     # now use the awesome context manager
     with ce.emit_ddl() as emitter:
-        #t1 = newtable.DocTable.from_schema(dummy_schema('t1'), ce)
-        #t2 = newtable.DocTable.from_schema(dummy_schema('t2'), ce)
+        #t1 = doctable.DocTable.from_schema(dummy_schema('t1'), ce)
+        #t2 = doctable.DocTable.from_schema(dummy_schema('t2'), ce)
         t1 = emitter.create_table(dummy_container('t1'))
         t2 = emitter.create_table(dummy_container2('t2'))
     print(t1, t2)
@@ -205,7 +205,7 @@ def test_new_doctable(test_table: str = 'test'):
     assert(len(ce.inspect_table_names()) == 2)
 
 def test_query(test_table: str = 'test1'):
-    ce = newtable.ConnectCore.open_new(
+    ce = doctable.ConnectCore.open_new(
         target = ':memory:', 
         dialect='sqlite',
     )
@@ -253,66 +253,66 @@ def test_query(test_table: str = 'test1'):
 
 def test_schema_definitions():
 
-    @newtable.table_schema
+    @doctable.table_schema
     class TestContainer0:
         id: int = None
         name: str = None
         age: int = None
 
     assert(dataclasses.is_dataclass(TestContainer0))
-    assert(newtable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer0))
-    schema = getattr(TestContainer0, newtable.SCHEMA_ATTRIBUTE_NAME)
+    assert(doctable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer0))
+    schema = getattr(TestContainer0, doctable.SCHEMA_ATTRIBUTE_NAME)
     assert(schema.table_name == 'TestContainer0')
 
     # test the most basic usage
-    @newtable.table_schema()
+    @doctable.table_schema()
     class TestContainer1:
         id: int
         name: str
         age: int
 
     assert(dataclasses.is_dataclass(TestContainer1))
-    assert(newtable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer1))
-    schema = getattr(TestContainer1, newtable.SCHEMA_ATTRIBUTE_NAME)
+    assert(doctable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer1))
+    schema = getattr(TestContainer1, doctable.SCHEMA_ATTRIBUTE_NAME)
     assert(schema.table_name == 'TestContainer1')
 
     # make sure the arguments work with it
     if sys.version_info.minor >= 10:
-        newtable.table_schema(TestDataClass, slots=True)
+        doctable.table_schema(TestDataClass, slots=True)
     else:
         try:
-            newtable.table_schema(TestDataClass, slots=True)
+            doctable.table_schema(TestDataClass, slots=True)
             raise Exception('Should have raised TypeError.')
         except TypeError as e:
             print(e)
 
-    @newtable.table_schema(
+    @doctable.table_schema(
         indices = {
-            'age_index': newtable.Index('age'),
+            'age_index': doctable.Index('age'),
         },
     )
     class TestContainer2:
         name: str
         age: int
-        id: int = newtable.Column(
-            column_args=newtable.ColumnArgs(order=0, primary_key=True, autoincrement=True),
+        id: int = doctable.Column(
+            column_args=doctable.ColumnArgs(order=0, primary_key=True, autoincrement=True),
         )
         
     
     assert(dataclasses.is_dataclass(TestContainer2))
-    assert(newtable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer2))
-    schema = getattr(TestContainer2, newtable.SCHEMA_ATTRIBUTE_NAME)
+    assert(doctable.SCHEMA_ATTRIBUTE_NAME in dir(TestContainer2))
+    schema = getattr(TestContainer2, doctable.SCHEMA_ATTRIBUTE_NAME)
     assert(schema.table_name == 'TestContainer2')
     
-    @newtable.table_schema(
+    @doctable.table_schema(
         table_name='mytable',
     )
     class TestContainer3:
         name: str
         age: int
-        id: int = newtable.Column(column_args=newtable.ColumnArgs(order=0, primary_key=True, autoincrement=True))
+        id: int = doctable.Column(column_args=doctable.ColumnArgs(order=0, primary_key=True, autoincrement=True))
     
-    schema = getattr(TestContainer3, newtable.SCHEMA_ATTRIBUTE_NAME)
+    schema = getattr(TestContainer3, doctable.SCHEMA_ATTRIBUTE_NAME)
     assert(schema.table_name == 'mytable')
 
     tc = TestContainer3(id=1, name='a', age=10)
