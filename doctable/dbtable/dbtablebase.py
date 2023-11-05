071c3406a277d8f73751c68cd99f497db2066131
Devin J. Cornell <devincornell@github.com>
2023-11-05 11:00:30 -0500

    fixed inspection methods

--- doctable/dbtable/dbtablebase.py
+++ doctable/dbtable/dbtablebase.py
@@ -21,26 +21,25 @@ class DBTableBase:
     def table_name(self) -> str:
         return self.table.name
 
+    def __getitem__(self, key: Any) -> sqlalchemy.Column:
+        '''Get a column by name.'''
+        return self.table.c[key]
+
     def cols(self, *columns) -> typing.List[sqlalchemy.Column]:
         '''Return a query with only the specified columns.'''
         return [self.table.c[col] if isinstance(col, str) else col for col in columns]
     
     def all_cols(self) -> typing.List[sqlalchemy.Column]:
         return [c for c in self.table.columns]
-    
-    def __getitem__(self, key: Any) -> sqlalchemy.Column:
-        '''Get a column by name.'''
-        return self.table.c[key]
-    
+        
+    def inspect_columns(self) -> sqlalchemy.engine.Inspector:
+        '''Get engine for this inspector.
+        '''
+        return self.core.inspect_columns(self.name)
+
     def inspect_indices(self) -> typing.List[typing.Dict[str, typing.Any]]:
         '''Wraps Inspector.get_indexes(tabname).'''
-        return self.inspect().get_indexes()
-
-    def inspect(self) -> sqlalchemy.engine.Inspector:
-        '''Get engine for this inspector.
-        https://docs.sqlalchemy.org/en/14/core/reflection.html#sqlalchemy.engine.reflection.Inspector
-        '''
-        return sqlalchemy.inspect(self.table)
+        return self.core.inspect_indices(self.name)
     
     @property
     def name(self) -> str:
