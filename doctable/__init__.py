721d3461e0d84abd10ea762f15b77334faa20588
Devin J. Cornell <devincornell@github.com>
2023-11-12 12:45:28 -0500

    more transparent interface to sqlalchemy expressions and functions

--- doctable/__init__.py
+++ doctable/__init__.py
@@ -8,25 +8,36 @@ name = "doctable"
 # expose some features of sqlalchemy
 import sqlalchemy
 
-class f:
-    max = sqlalchemy.sql.func.max
-    min = sqlalchemy.sql.func.min
-    count = sqlalchemy.sql.func.count
-    sum = sqlalchemy.sql.func.sum
+f = sqlalchemy.sql.func
+''' See docs for more details:
+https://docs.sqlalchemy.org/en/20/core/functions.html#selected-known-functions
+'''
 
-    distinct = sqlalchemy.sql.expression.distinct
-    between = sqlalchemy.sql.expression.between
-    
-    all_ = sqlalchemy.sql.expression.all_
-    and_ = sqlalchemy.sql.expression.and_
-    or_ = sqlalchemy.sql.expression.or_
-    not_ = sqlalchemy.sql.expression.not_
+exp = sqlalchemy.sql.expression
+''' See sqlalchemy docs for more details.
+TODO: get link
+'''
 
-    desc = sqlalchemy.sql.expression.desc
-    asc = sqlalchemy.sql.expression.asc
-    
-    any_ = sqlalchemy.sql.expression.any_
-    alias = sqlalchemy.sql.expression.alias
-    between = sqlalchemy.sql.expression.between
+#class f:
+#    #sqlalchemy.sql.func
+#    max = sqlalchemy.sql.func.max
+#    min = sqlalchemy.sql.func.min
+#    count = sqlalchemy.sql.func.count
+#    sum = sqlalchemy.sql.func.sum
+#
+#    distinct = sqlalchemy.sql.expression.distinct
+#    between = sqlalchemy.sql.expression.between
+#    
+#    all_ = sqlalchemy.sql.expression.all_
+#    and_ = sqlalchemy.sql.expression.and_
+#    or_ = sqlalchemy.sql.expression.or_
+#    not_ = sqlalchemy.sql.expression.not_
+#
+#    desc = sqlalchemy.sql.expression.desc
+#    asc = sqlalchemy.sql.expression.asc
+#    
+#    any_ = sqlalchemy.sql.expression.any_
+#    alias = sqlalchemy.sql.expression.alias
+#    between = sqlalchemy.sql.expression.between
 
 
