79ad7ecc98657973a7062349705d70dc0b3de688
Devin J. Cornell <devincornell@github.com>
2023-11-15 07:01:14 -0500

    moved exposure of sqlalchemy funcs and expressions to separate file

--- doctable/__init__.py
+++ doctable/__init__.py
@@ -1,22 +1,13 @@
+
+name = "doctable"
+
 from .connectcore import ConnectCore, TableAlreadyExistsError, TableDoesNotExistError
 from .query import *
 from .schema import *
 from .dbtable import *
+from .exposed import *
 
-name = "doctable"
 
-# expose some features of sqlalchemy
-import sqlalchemy
-
-f = sqlalchemy.sql.func
-''' See docs for more details:
-https://docs.sqlalchemy.org/en/20/core/functions.html#selected-known-functions
-'''
-
-exp = sqlalchemy.sql.expression
-''' See sqlalchemy docs for more details.
-TODO: get link
-'''
 
 #class f:
 #    #sqlalchemy.sql.func
---
+++ doctable/exposed.py
@@ -0,0 +1,15 @@
+
+
+# expose some features of sqlalchemy
+import sqlalchemy
+
+f = sqlalchemy.sql.func
+''' See docs for more details:
+https://docs.sqlalchemy.org/en/20/core/functions.html#selected-known-functions
+'''
+
+exp = sqlalchemy.sql.expression
+''' See sqlalchemy docs for more details.
+TODO: get link
+'''
+
